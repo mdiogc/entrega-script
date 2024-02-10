c2d1fe984f0f82ffa044eab790fcc0dcb3e9e1bc
Kai <145053705+YoooKai@users.noreply.github.com>
2024-02-10 12:32:52 +0000

    Update script-entrega.sh

--- script-entrega.sh
+++ script-entrega.sh
@@ -1,3 +1,94 @@
+
+###########################################
+#
+# Autor: Kai Rodríguez García
+#
+# Nombre: EjercicioSoftware1.sh
+#
+# Objetivo: Debe recibir un argumento (nombre del paquete), si el usuario no lo indica, se le pedirá luego por teclado hasta que especifique un nombre.
+#
+# Si el paquete indicado NO ESTÁ INSTALADO en el sistema, se comprobará si existe en los repositorios del sistema:
+# Si el paquete SÍ EXISTE en los repositorios: se le mostrará la información del paquete y se le dará la opción de instalarlo.
+# Si el paquete NO EXISTE en los repositorios: se le indicará al usuario que no hay ningún paquete que se llame como ha indicado,
+# y se le mostrará el resultado de la búsqueda que se obtiene con el argumento que ha dado el usuario
+# (puede que la búsqueda no dé ningún paquete, o puede que la búsqueda muestre paquetes que se llaman de forma similar. En cualquier caso, se mostrará el resultado).
+#
+# Si el paquete indicado SÍ ESTÁ INSTALADO: se le dará la opción de:
+# Mostrar su versión
+# Reinstalarlo
+# Actualizarlo (solo este paquete, si fuera actualizable)
+# Eliminarlo (guardando la configuración)
+# Eliminarlo totalmente
+#
+# Entradas: nombre de un paquete
+# Salidas: mostrar info del paquete, reinstalarlo, actualizarlo, eliminarlo
+#
+# Historial: 7/02/2024 #Primer esqueleto del ejercicio
+#
+###########################################
+
+paquete=$1
+
+while [ -z "$paquete" ]; do
+    echo "ERROR: No ha escrito un argumento"
+    read -p "Escribe el nombre del paquete: " paquete
+done
+
+echo "Espera un momento mientras se actualizan los repositorios: "
+
+sudo apt update > /dev/null 2>&1
+
+echo "Los repositorios se han actualizado."
+echo ""
+
+dpkg -s $paquete > /dev/null 2>&1
+if [ "$?" -eq 1 ]; then
+    apt-cache search $paquete /dev/null 2>&1
+    if [ "$?" -eq 0 ]; then
+        echo "La información sobre este paquete es la siguiente: "
+        apt-cache policy $paquete
+        read -p "¿Quiere instalar este programa? (S/N)" preguntainstalar
+        case $preguntainstalar in
+        "S") sudo apt-get install $paquete ;;
+        "N") ;;
+        esac
+    else
+        echo "No está disponible o no existe ese paquete"
+        echo "El resultado de la búsqueda es el siguiente: "
+        echo ""
+        apt search $paquete
+        read -p "Escriba de nuevo el nombre exacto del paquete " paquete
+
+    fi
+else
+    echo "                *********************"
+    echo "                *******Menú**********"
+    echo "                *********************"
+    echo ""
+    echo "┌───────────────────────────────────────────────────────────┐"
+    echo ""
+    echo "1) Mostrar Versión"
+    echo "2) Reinstalar"
+    echo "3) Actualizarlo"
+    echo "4) Eliminarlo (guardando la configuración)"
+    echo "5) Eliminarlo totalmente"
+    echo ""
+    echo "└───────────────────────────────────────────────────────────┘"
+    read -p "Elija la opción que desee: " opcion
+
+    case "$opcion" in
+    1) dpkg -s $paquete ;;
+    2) sudo apt-get reinstall $paquete ;;
+    3) sudo apt-get upgrade $paquete ;;
+    4) sudo apt-get remove $paquete ;;
+    5) sudo apt-get purge $paquete ;;
+    *) echo "Error: '$opcion' no es una opción correcta" ;;
+    esac
+fi
+
+
+
+
 Obj:1 http://archive.ubuntu.com/ubuntu focal InRelease
 Obj:2 http://es.archive.ubuntu.com/ubuntu jammy InRelease                                                                          
 Obj:3 http://es.archive.ubuntu.com/ubuntu jammy-updates InRelease                                                                  
